7c33fe4214faf1a44e8c19418247aab23d5a570b
vladtischenko <tischenkovlad@gmail.com>
2014-04-28 00:44:46 +0300

    rating_fields_change

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,8 +5,5 @@ class Rating < ActiveRecord::Base
   validates :rate, :text, :title, :book_id, :user_id, presence: true
   validates :rate, numericality: true, inclusion: {in: 1..5}
 
-  mount_uploader :grey_img, PictureUploader
-  mount_uploader :yellow_img, PictureUploader
-
   scope :not_allowed, -> { where(allow: false) }
 end
